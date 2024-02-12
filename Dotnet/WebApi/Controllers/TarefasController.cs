398073536362b7c41df21aed8ad69d2027c88c06
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-12 16:08:49 -0300

    WebApi

---
+++ Dotnet/WebApi/Controllers/TarefasController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
+
+namespace TrilhaApiDesafio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly OrganizadorContext _context;
+
+        public TarefasController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefas()
+        {
+            return await _context.Tarefas.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tarefa>> GetTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return tarefa;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarefa(int id, Tarefa tarefa)
+        {
+            if (id != tarefa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tarefa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarefaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tarefa>> PostTarefa(Tarefa tarefa)
+        {
+            _context.Tarefas.Add(tarefa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarefa", new { id = tarefa.Id }, tarefa);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tarefas.Remove(tarefa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TarefaExists(int id)
+        {
+            return _context.Tarefas.Any(e => e.Id == id);
+        }
+    }
+}
